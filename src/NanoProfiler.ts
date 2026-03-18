84d5fd29da3e566cd3c7ec9597e3efc1ce2c243c
komed3 <pk97@outlook.de>
2026-03-18 23:40:59 +0100

    instantiate NanoProfiler

--- src/NanoProfiler.ts
+++ src/NanoProfiler.ts
@@ -1,9 +1,24 @@
+export interface ProfilerOptions {}
+
+export interface ProfilerHooks {}
+
 export type RunnerFn< T > = ( fn: () => T, label?: string, meta?: any ) => T;
 export type AsyncRunnerFn< T > = ( fn: () => Promise< T >, label?: string, meta?: any ) => Promise< T >;
 
 export class NanoProfiler {
 
+    private static globalInstance?: NanoProfiler;
+
+    public static global () : NanoProfiler {
+        return this.globalInstance ??= new NanoProfiler ();
+    }
+
     private runner: RunnerFn< any >;
     private runnerAsync: AsyncRunnerFn< any >;
+
+    constructor (
+        private readonly options: ProfilerOptions = {},
+        private readonly hooks?: ProfilerHooks
+    ) {}
 
 }
