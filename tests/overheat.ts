99f7a9318516cab8dc90584d3d902049cac10cc1
komed3 <pk97@outlook.de>
2026-03-20 00:06:13 +0100

    min fix

--- tests/overheat.ts
+++ tests/overheat.ts
@@ -2,7 +2,7 @@ import { NanoProfiler } from '../src/NanoProfiler.ts';
 
 const profiler = NanoProfiler.global();
 
-for ( let i = 0; i < 1e6; i++ ) profiler.run( Function );
+for ( let i = 0; i < 10_000; i++ ) profiler.run( Function );
 
 console.log( profiler.summary() );
 console.log( profiler.histogram() );
