3d6c63b36227eafa415d8e0327a3f7c2394ecf31
komed3 <pk97@outlook.de>
2026-03-19 10:06:55 +0100

    add profiler tests

---
+++ tests/profile.ts
@@ -0,0 +1,24 @@
+import { NanoProfiler } from '../src/NanoProfiler.ts';
+
+const profiler = NanoProfiler.global();
+
+const workload_1 = () => { for ( let i = 0; i < 1e6; i++ ) Math.sqrt( i ) };
+const workload_2 = () => { for ( let i = 0; i < 1e6; i++ ) Math.log( i + 1 ) };
+const workload_3 = () => { for ( let i = 0; i < 1e6; i++ ) Math.sin( i ) };
+
+profiler.run( workload_1, 'sqrt' );
+profiler.run( workload_2, 'log' );
+profiler.run( workload_3, 'sin' );
+profiler.run( workload_1, 'sqrt' );
+profiler.run( workload_2, 'log' );
+profiler.run( workload_3, 'sin' );
+profiler.run( workload_3, 'sin' );
+profiler.run( workload_1, 'sqrt' );
+profiler.run( workload_3, 'sin' );
+profiler.run( workload_2, 'log' );
+profiler.run( workload_1, 'sqrt' );
+profiler.run( workload_2, 'log' );
+
+console.log( 'Summary for sqrt:', profiler.summary( 'sqrt' ) );
+console.log( 'Hotspot for log:', profiler.hotspot( 'log' ) );
+console.log( 'Histogram:', profiler.histogram( undefined, 5 ) );
