a9ae7aa36b29ccb70f2279d12e94e54649d913b2
komed3 <pk97@outlook.de>
2026-03-19 10:15:34 +0100

    update tests

--- tests/profile.ts
+++ tests/profile.ts
@@ -4,22 +4,24 @@ const profiler = new NanoProfiler ( {
     onEntry ( entry ) { console.log( `Entry: ${ entry.label }, Time: ${ entry.time }ms, Mem: ${ entry.mem }MB` ) }
 } );
 
-const workload_1 = () => { for ( let i = 0; i < 1e6; i++ ) Math.sqrt( i ) };
-const workload_2 = () => { for ( let i = 0; i < 1e6; i++ ) Math.log( i + 1 ) };
-const workload_3 = () => { for ( let i = 0; i < 1e6; i++ ) Math.sin( i ) };
+const workload_1 = () => { for ( let i = 0; i < 1e3; i++ ) Math.sqrt( i ) };
+const workload_2 = () => { for ( let i = 0; i < 1e3; i++ ) Math.log( i + 1 ) };
+const workload_3 = () => { for ( let i = 0; i < 1e3; i++ ) Math.sin( i ) };
 
-profiler.run( workload_1, 'sqrt' );
-profiler.run( workload_2, 'log' );
-profiler.run( workload_3, 'sin' );
-profiler.run( workload_1, 'sqrt' );
-profiler.run( workload_2, 'log' );
-profiler.run( workload_3, 'sin' );
-profiler.run( workload_3, 'sin' );
-profiler.run( workload_1, 'sqrt' );
-profiler.run( workload_3, 'sin' );
-profiler.run( workload_2, 'log' );
-profiler.run( workload_1, 'sqrt' );
-profiler.run( workload_2, 'log' );
+for ( let i = 0; i < 10; i++ ) {
+    profiler.run( workload_1, 'sqrt' );
+    profiler.run( workload_2, 'log' );
+    profiler.run( workload_3, 'sin' );
+    profiler.run( workload_1, 'sqrt' );
+    profiler.run( workload_2, 'log' );
+    profiler.run( workload_3, 'sin' );
+    profiler.run( workload_3, 'sin' );
+    profiler.run( workload_1, 'sqrt' );
+    profiler.run( workload_3, 'sin' );
+    profiler.run( workload_2, 'log' );
+    profiler.run( workload_1, 'sqrt' );
+    profiler.run( workload_2, 'log' );
+}
 
 console.log( 'Summary for sqrt:', profiler.summary( 'sqrt' ) );
 console.log( 'Hotspot for log:', profiler.hotspot( 'log' ) );
