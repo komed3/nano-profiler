b624c323768a3685130145df3ac6352eb18e5bb0
komed3 <pk97@outlook.de>
2026-03-21 01:02:34 +0100

    use prettier

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,17 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import cleanup from 'rollup-plugin-cleanup';
+import prettier from 'rollup-plugin-prettier';
 
 export default {
     input: 'src/NanoProfiler.ts',
     plugins: [
-        resolve(), commonjs(),
+        resolve( { extensions: [ '.js', '.ts' ] } ), commonjs(),
+        typescript( { tsconfig: 'tsconfig.json', compilerOptions: { declaration: false } } ),
         cleanup( { comments: 'istanbul', extensions: [ 'js', 'ts' ] } ),
-        typescript( {
-            tsconfig: 'tsconfig.json', clean: true,
-            tsconfigOverride: { compilerOptions: { declaration: false } }
+        prettier( {
+            parser: 'babel', tabWidth: 2, bracketSpacing: true, bracketSameLine: true, singleQuote: true,
+            jsxSingleQuote: true, trailingComma: 'none', objectWrap: 'collapse'
         } )
     ],
     output: [ {
